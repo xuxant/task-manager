b6c0337e54f14deee443bb40b720c91efb722d91
Susanta Gautam <xuxant02@gmail.com>
2019-12-29 17:23:48 +0000

    Task apis

---
+++ src/db/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/tasks-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+
