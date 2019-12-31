950521b98779aaf0083fcf754efe26f5ed77d322
Susanta Gautam <xuxant02@gmail.com>
2019-12-31 14:30:37 +0000

    Routers

--- src/index.js
+++ src/index.js
@@ -1,118 +1,15 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
 
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e){
-        res.status(400).send(e)
-    }
-
-})
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-
-    try{ 
-        await task.save()
-        res.status(201).send(task)
-    } catch(e) {
-        res.status(400).send(e)
-    }
-})
-
-app.get('/users', async (req,res) => {
-    try{
-        const users = await User.find({})
-        res.send(users)
-    } catch(e) {
-        return res.status(500).send(e)
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e) {
-        return res.status(500).send(e)
-    }
-})
-
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name','email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if(!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid Operation' })
-    }
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e){
-        res.status(400).send(e)
-    }
-})
-
-app.patch('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'completed']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if(!isValidOperation) {
-        return res.status(404).send({ error: 'Invalid Task' })
-    }
-
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if(!task){
-            return res.status(404).send({ error: 'Task Not Found' })
-        }
-        res.status(201).send(task)
-    } catch(e) {
-        res.status(500).send({ error: 'Internal Server Error' })
-    }
-})
-
-app.get('/tasks', async (req, res) => {
-
-    try{
-        const task = await Task.find({})
-        res.send(task)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const task = await Task.findById(_id)
-        res.send(task)
-    } catch(e) {
-        res.status(500).send(e)
-    }
-})
 
 app.listen(port, () => {
     console.log('Server is listining on port ' + port)
