bfd8e4e57224a666236495a0c6968b0cbe622e70
Susanta Gautam <xuxant02@gmail.com>
2020-01-01 16:32:46 +0000

    JWT Tokens

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if(req.method === 'GET') {
+//         res.send('GET request are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -14,4 +22,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is listining on port ' + port)
 })
-
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
-
+const auth = require('../middleware/auth')
 // Insert data into user collection
 
 router.post('/users', async (req, res) => {
@@ -9,20 +9,39 @@ router.post('/users', async (req, res) => {
     
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken ()
+        res.status(201).send({ user, token })
     } catch (e){
         res.status(400).send(e)
     }
 
 })
 
-router.get('/users', async (req,res) => {
+router.post('/users/login', async (req, res) => {
     try{
-        const users = await User.find({})
-        res.send(users)
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken();
+        res.send({ user, token })
     } catch(e) {
-        return res.status(500).send(e)
+        res.status(400).send(e)
     }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    }catch(e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth , async (req,res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
@@ -61,7 +80,12 @@ router.patch('/users/:id', async (req, res) => {
         return res.status(400).send({ error: 'Invalid Operation' })
     }
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+        
+        updates.forEach((update) => user[update] = req.body[update] )
+        await user.save()
+
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
         if (!user) {
             return res.status(404).send()
         }
